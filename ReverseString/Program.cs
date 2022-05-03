b348db1dafb69bd9e0a7cf83cbdad23b720c7c2e
slavCode <stomow@gmail.com>
2022-05-03 10:53:29 +0300

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            var reversed = text.Reverse();
+            Console.WriteLine(String.Join("", reversed));
+        }
+    }
+}
