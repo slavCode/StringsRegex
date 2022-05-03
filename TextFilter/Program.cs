8ff2c65fc2fc537f29d7e09caaaf137e34f9ba46
slavCode <stomow@gmail.com>
2022-05-03 13:03:26 +0300

    Add Text Filter

---
+++ TextFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var text = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                var censoredWord = "";
+                for (int i = 0; i < word.Length; i++) censoredWord += '*';
+
+                text = text.Replace(word, censoredWord);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
