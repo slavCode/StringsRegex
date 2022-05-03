d809a50a76831f6a4eb66a03f13b6ea348f27d3f
slavCode <stomow@gmail.com>
2022-05-03 11:19:17 +0300

    Add 1 and 2 task

---
+++ CountSubstringOccurrences/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountSubstringOccurrences
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine().ToLower();
+            string pattern = Console.ReadLine().ToLower();
+
+            int counter = 0;
+            int index = text.IndexOf(pattern);
+
+            while (index != -1)
+            {
+                counter++;
+                index = text.IndexOf(pattern, index + 1);
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
