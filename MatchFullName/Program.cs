de9d1220c8967e6df04e230810105ea2b31acff5
Slav Tomov <stomow@outlook.com>
2022-05-05 19:38:16 +0300

    Add Match full name and Match phone number

---
+++ MatchFullName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            Regex regex = new Regex(pattern);
+
+            bool isMatch = regex.IsMatch(text);
+            Console.WriteLine(isMatch);
+        }
+    }
+}
---
+++ MatchPhoneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string pattern = @"(\+359-\d-\d{3}-\d{4}\b)|(\+359 \d \d{3} \d{4}\b)";
+            string text = Console.ReadLine();
+
+            var matches = Regex.Matches(text, pattern);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
