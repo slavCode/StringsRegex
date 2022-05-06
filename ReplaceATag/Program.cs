d0e92961f3123b37e03e01e5657ad6a5295bb775
Slav Tomov <stomow@outlook.com>
2022-05-06 10:07:33 +0300

    Add Replace Tag

---
+++ ReplaceATag/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ReplaceATag
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string replacment = $@"[URL href=$1]$2[/URL]";
+                string pattern = @"<a.*?href.*?""(.*)"">(.*?)<\/a>";
+                string replaced = Regex.Replace(input, pattern, replacment);
+
+                Console.WriteLine(replaced);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
